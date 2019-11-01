7af4e89565990eaed60dd24641b7f275e21eb77c
Eduardo Neira <edun327@gmail.com>
2019-11-01 00:39:02 -0300

    Release 0.2.0

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", 'r') as f:
 
 setup(
    name='big-fiubrother-display',
-   version='0.1.0',
+   version='0.2.0',
    description='Big Fiubrother Display Application',
    license="GPLv3",
    long_description=long_description,
